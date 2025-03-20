6835787342620cca7cc316354e84d1d18c33a18e
marianacastanedar <167562546+marianacastanedar@users.noreply.github.com>
2025-03-20 00:05:01 -0600

    LispValue base

--- LispValue.java
+++ LispValue.java
@@ -1,4 +1,43 @@
-import java.util.List;
+import java.util.*;
+
+/**
+ * La clase LispValue implementa LispExpression y se uso para ver los datos
+ * de Lisp, como números, cadenas y listas.
+ */
 public class LispValue implements LispExpression {
-    
+    private Object value; // Un valor que puede ser varios tipos de datos de lisp como numeros cadenas o listas
+
+    // constructor para tener un lispvalue de algun valor
+    public LispValue(Object value) {
+        this.value = value;
+    }
+
+    @Override
+    public Object evaluate() { //devuelve el valor
+        return value; 
+    }
+
+    // Se aseegura si es una lista
+    public boolean isList() {
+        return value instanceof List;
+    }
+
+    // Se asegura si es un numero
+    public boolean isNumber() {
+        return value instanceof Number;
+    }
+
+    // Asegura si es un simnbolo
+    public boolean isSymbol() {
+        return value instanceof String;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    // para hacer el set del valor
+    public void setValue(Object value) {
+        this.value = value;
+    }
 }
